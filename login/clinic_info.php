2dae02686232150d32b3fd354debfb438db00233
Ezzat Abdel-Khalek <eabdelkh@emich.edu>
2023-11-28 09:37:36 -0500

    reverting changes

--- login/clinic_info.php
+++ login/clinic_info.php
@@ -61,7 +61,7 @@
             </tr>
             <?php
                 // Connect to database
-                $con = mysqli_connect("localhost", "root", "", "blood_clinic");
+                $con = mysqli_connect("localhost", "root", "", "blood clinic");
 
                 if ($con -> connect_error) {
                     die("Connection failed:" . $con -> connect_error);
